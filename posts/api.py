0ab29b97ce1037c1bde3bb73a674502173d5dd8a
michael reid <michaelreid@users.noreply.github.com>
2015-05-05 10:00:04 +0930

    Writting  and  testing and api endpoint

--- posts/api.py
+++ posts/api.py
@@ -11,5 +11,31 @@ from database import session
 @app.route("/api/posts", methods=["GET"])
 def posts_get():
     """  Get a list of posts """
-    data = json.dumps([])
+
+    # Get the posts from the database
+    posts = session.query(models.Post).all()
+
+    # Convert to json format
+    data = json.dumps([post.as_dictionary() for post in posts])
+    return Response(data, 200, mimetype="application/json")
+
+
+
+@app.route("/api/posts/<int:id>", methods=["GET"])
+def post_get(id):
+    """  Single post endpoint """
+
+    # Get post with <id> from database
+    post = session.query(models.Post).get(id)
+
+
+    # Check if post exists
+    # If not return 404 with a helpful message
+    if not post:
+        message = "Could not find post with id {}".format(id)
+        data = json.dumps({"message": message})
+        return Response(data, 404, mimetype="application/json")
+
+    # Else return post
+    data = json.dumps(post.as_dictionary())
     return Response(data, 200, mimetype="application/json")
--- tests/api_tests.py
+++ tests/api_tests.py
@@ -40,6 +40,8 @@ class TestAPI(unittest.TestCase):
     def testGetPosts(self):
         """ Getting posts from a populated database """
 
+
+        # First define test posts and commit them to database
         postA = models.Post(title="Example Post A", body="Just a test")
         postB = models.Post(title="Example Post B", body="Another test")
 
@@ -47,10 +49,64 @@ class TestAPI(unittest.TestCase):
         session.commit()
 
         
+        # Then retrieve the posts using the api endpoint
 
+            # Define response as from the endpoint testing
+        response = self.client.get("/api/posts")
+            # Test response is successful and mimetype correct
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.mimetype, "application/json")
+            # Test two posts returned
+        data = json.loads(response.data)
+        self.assertEqual(len(data), 2)
+            # Test two posts are same as exist in database
+        postA = data[0]
+        self.assertEqual(postA["title"], "Example Post A")
+        self.assertEqual(postA["body"], "Just a test")
 
+        postB = data[1]
+        self.assertEqual(postB["title"], "Example Post B")
+        self.assertEqual(postB["body"], "Another test")
+        
+        
+    # Testing that api can return single post
 
+    def testGetPost(self):
+        """ Getting single post from database """
 
+        # First define test posts and commit to database
+        postA = models.Post(title="Example Post A", body="Just a test")
+        postB = models.Post(title="Example Post B", body="Another test")
+        
+        session.add_all([postA, postB])
+        session.commit()
+
+        # Then retrieve from database and test if correct
+        response = self.client.get("/api/posts/{}".format(postB.id))
+
+            # - correct response and correct mimetype
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.mimetype, "application/json")
+
+            # - correct data in single post
+        post = json.loads(response.data)
+        self.assertEqual(post["title"],"Example Post B")
+        self.assertEqual(post["body"], "Another test")
+
+            
+    # Testing correct response for nonexistant post
+
+    def testNonExistantPost(self):
+        """ Getting a single post which does not exist """
+        response = self.client.get("/api/posts/1")
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.mimetype, "application/json")
+
+        data = json.loads(response.data)
+        self.assertEqual(data["message"], "Could not find post with id 1")
+
+       
 
 
     def tearDown(self):
